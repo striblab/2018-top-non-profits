be5776560ad5a242dd982de4972a6c7c186b4da3
Alan Palazzolo <alan.palazzolo@startribune.com>
2018-09-27 16:29:01 -0500

    Inital template and data parts.

---
+++ nonprofits/contact-list.js
@@ -0,0 +1,102 @@
+/**
+ * Create contact list
+ */
+
+// Dependencies
+const queryString = require('query-string');
+const _ = require('lodash');
+const GoogleURL = require('google-url');
+const parseName = require('parse-full-name').parseFullName;
+let { getData } = require('../lib/gulp-html.js');
+
+// Main function
+async function main() {
+  let sources = await getData();
+
+  // Make sure we don't have any that are dropped
+  let list = _.filter(sources.contactList, c => {
+    return !c.companytype.match(/drop/i);
+  });
+
+  // Parse list
+  list = _.map(list, c => {
+    // Parse name
+    let name = parseName(c.contact);
+    if (name.error && name.error.length) {
+      console.error('Name parse error: ' + name.error);
+    }
+    delete name.error;
+
+    // Put together list data
+    return _.extend(name, {
+      contact: c.contact,
+      phone: c.contactphone,
+      email: c.contactemail,
+      orgID: c.coid,
+      orgIRSNo: c.irsno,
+      orgName: c.name,
+      orgType: c.companytype,
+      orgAddressFull: _.filter([
+        c.address1 + (c.address2 ? ' ' + c.address2 : ''),
+        c.city,
+        c.state + ' ' + c.zip
+      ]).join(', '),
+      orgWebsite: c.www,
+      orgDescription: c.sescription,
+      orgShortDescription: c.shortdesc
+    });
+  });
+
+  // Some checks
+  _.each(list, c => {
+    if (!c.email) {
+      console.error(`${c.orgID}: ${c.orgName} does not have a contact email.`);
+    }
+    if (!c.contact) {
+      console.error(`${c.orgID}: ${c.orgName} does not have a contact person.`);
+    }
+  });
+
+  // Make link
+  for (let c of list) {
+    c.surveyURL = `${
+      process.env.SURVEY_URL
+    }viewform?usp=pp_url&${queryString.stringify({
+      'entry.463322031': process.env.SURVEY_PASS,
+      'entry.311536750': c.orgID,
+      'entry.646452484': c.orgName,
+      'entry.1440568490': c.orgIRSNo,
+      'entry.1326466709': c.orgAddressFull,
+      'entry.1296712022': c.orgWebsite,
+      'entry.947743576': c.orgShortDescription,
+      'entry.616715940': c.orgDescription,
+      'entry.588271044': c.contact,
+      'entry.1899543844': c.email,
+      'entry.1414459639': c.phone
+    })}`;
+
+    // // Mailchimp only allows for 255 chracters (255 bytes)
+    // // https://kb.mailchimc.com/lists/growth/format-guidelines-for-your-import-file
+    // if (cache.urls && cache.urls[c.surveyURL]) {
+    //   c.surveyURLShort = cache.urls[c.surveyURL];
+    //   return done(null, p);
+    // }
+
+    // shortener.shorten(c.surveyURL, (error, short) => {
+    //   if (error) {
+    //     return done(error);
+    //   }
+
+    //   c.surveyURLShort = short;
+    //   cache.urls = cache.urls || {};
+    //   cache.urls[c.surveyURL] = short;
+    //   writeCache();
+    //   done(null, p);
+    // });
+  }
+
+  console.log(list);
+}
+
+// Run main
+main();
---
+++ nonprofits/data.js
@@ -0,0 +1,56 @@
+/**
+ * Data sources used by the build data process.
+ */
+
+// Dependencies
+const configUtil = require('../lib/config.js');
+require('dotenv').load();
+
+// Get config for output
+let { config } = configUtil.getConfig();
+
+// Main export is the config for buildData
+module.exports = {
+  // Nonprofit list based on finance data available, used for
+  // publication
+  nonprofits: {
+    source: `${
+      process.env.DATA_UI_URI
+    }/api/v01/company_details/?nonprofit_finance_publishyear=${
+      config.data ? config.data.publishYear.data : '2017'
+    }&limit=100&username=${process.env.DATA_UI_USERNAME}&api_key=${
+      process.env.DATA_UI_API_KEY
+    }`,
+    type: 'json',
+    postprocess: parseNonprofits,
+    ttl: 1000 * 60 * 60 * 20
+  },
+  // List used for mailing
+  contactList: {
+    source: `${
+      process.env.DATA_UI_URI
+    }/api/v01/company/?companytype__iregex=charity|foundation|nonprofit|trust&contactemail__iregex=@&state=MN&username=${
+      process.env.DATA_UI_USERNAME
+    }&api_key=${process.env.DATA_UI_API_KEY}`,
+    postprocess: parseContactList,
+    ttl: 1000 * 60 * 60 * 20
+  }
+};
+
+// Parse nonprofits
+function parseNonprofits(input) {
+  if (input && input.objects) {
+    return input.objects;
+  }
+
+  return {};
+}
+
+// Parse contact list
+function parseContactList(input) {
+  if (input && input.objects) {
+    return input.objects;
+  }
+
+  return {};
+}
