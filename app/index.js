762c15cb74a00acfcd5db8f32b16331d8bcd4424
Alan Palazzolo <alan.palazzolo@startribune.com>
2018-11-15 15:29:05 -0600

    Initial copy of 2017 markup and styles.

--- app/index.js
+++ app/index.js
@@ -5,48 +5,36 @@
 // Define globals that are added through the js.globals in
 // the config.json file, here, mostly so linting won't get triggered
 // and its a good queue of what is available:
-// /* global _ */
+/* global $ */
 
 // Dependencies
 import utils from './utils.js';
+import Content from '../templates/_index-content.svelte.html';
+import nonprofits from '../assets/data/nonprofits.json';
 
 // Mark page with note about development or staging
 utils.environmentNoting();
 
+// Hacky way to get the share parts to show up
+let $share = $('.share-placeholder').size()
+  ? $('.share-placeholder')
+    .children()
+    .detach()
+  : undefined;
+let attachShare = !$share
+  ? undefined
+  : () => {
+    $('.share-placeholder').append($share);
+  };
 
-
-// Adding dependencies
-// ---------------------------------
-// Import local ES6 or CommonJS modules like this:
-// import utilsFn from './utils.js';
-//
-// Or import libraries installed with npm like this:
-// import module from 'module';
-
-// Adding Svelte templates in the client
-// ---------------------------------
-// We can bring in the same Svelte templates that we use
-// to render the HTML into the client for interactivity.  The key
-// part is that we need to have similar data.
-//
-// First, import the template.  This is the main one, and will
-// include any other templates used in the project.
-// import Content from '../templates/_index-content.svelte.html';
-//
-// Get the data parts that are needed.  There are two ways to do this.
-// If you are using the buildData function to get data, then ?
-//
-// 1. For smaller datasets, just import them like other files.
-// import content from './assets/data/content.json';
-//
-// 2. For larger data points, utilize window.fetch.
-// let content = await (await window.fetch('./assets/data/content.json')).json();
-//
-// Once you have your data, use it like a Svelte component:
-//
-// const app = new Content({
-//   target: document.querySelector('.article-lcd-body-content'),
-//   data: {
-//     content
-//   }
-// });
+// Svelte template hook-up
+const app = new Content({
+  target: document.querySelector('.article-lcd-body-content'),
+  data: {
+    nonprofits,
+    // Would be nice to be able to pull this in from config.json
+    publishYear: 2017,
+    attachShare
+  }
+});
+window.__app = app;
--- nonprofits/data.js
+++ nonprofits/data.js
@@ -3,11 +3,18 @@
  */
 
 // Dependencies
+const path = require('path');
+const fs = require('fs');
+const _ = require('lodash');
 const configUtil = require('../lib/config.js');
 require('dotenv').load();
 
 // Get config for output
 let { config } = configUtil.getConfig();
+
+// Places for images
+const logoPath = path.join(__dirname, '..', 'assets', 'logos');
+const ceoPath = path.join(__dirname, '..', 'assets', 'ceos');
 
 // Main export is the config for buildData
 module.exports = {
@@ -23,6 +30,7 @@ module.exports = {
     }`,
     type: 'json',
     postprocess: parseNonprofits,
+    local: 'nonprofits.json',
     ttl: 1000 * 60 * 60 * 20
   },
   // List used for mailing
@@ -37,13 +45,120 @@ module.exports = {
   }
 };
 
-// Parse nonprofits
+// Parse nonprofits, clean up and remove unnecessary data
 function parseNonprofits(input) {
-  if (input && input.objects) {
-    return input.objects;
+  if (!input || !input.objects) {
+    return {};
   }
 
-  return {};
+  let publishYear = 2017; //config.data ? config.data.publishYear.data : 2017;
+  let validYears = [publishYear, publishYear - 1];
+  let parsed = input.objects;
+
+  parsed = _.map(parsed, p => {
+    delete p.notes;
+
+    // Limit employees
+    if (p.employees && p.employees.length) {
+      p.employees = _.filter(p.employees, d => {
+        return ~validYears.indexOf(d.publishyear);
+      });
+    }
+
+    // Limit finances
+    if (p.nonprofit_finances && p.nonprofit_finances.length) {
+      p.nonprofit_finances = _.filter(p.nonprofit_finances, d => {
+        return ~validYears.indexOf(d.publishyear);
+      });
+    }
+
+    // Limit officers
+    if (p.officers && p.officers.length) {
+      p.officers = _.map(p.officers, o => {
+        if (o.nonprofit_salaries && o.nonprofit_salaries.length) {
+          o.nonprofit_salaries = _.filter(o.nonprofit_salaries, d => {
+            return ~validYears.indexOf(d.publishyear);
+          });
+        }
+
+        return o;
+      });
+    }
+
+    return p;
+  });
+
+  // Filter any ones we dont have data for
+  parsed = _.filter(parsed, p => {
+    return (
+      p.nonprofit_finances &&
+      !!_.find(p.nonprofit_finances, { publishyear: publishYear })
+    );
+  });
+
+  // Check for images
+  parsed = _.map(parsed, p => {
+    p.hasLogo = fs.existsSync(path.join(logoPath, `${p.coid}.png`));
+
+    p.officers = _.map(p.officers, o => {
+      o.hasImage = fs.existsSync(path.join(ceoPath, `${o.id}.png`));
+      return o;
+    });
+
+    return p;
+  });
+
+  // Category names
+  parsed = _.map(parsed, p => {
+    _.find(
+      {
+        'Social services': /social.*/i,
+        Healthcare: /health.*care/i,
+        Arts: /art/i,
+        Education: /education/i,
+        Other: /.*/
+      },
+      (match, cat) => {
+        if (p.category && p.category.match(match)) {
+          p.category = cat;
+          return true;
+        }
+      }
+    );
+
+    return p;
+  });
+
+  // Clean up
+  parsed = _.map(parsed, p => {
+    return pruneEmpty(p);
+  });
+
+  // Sort and rank previous year
+  parsed = _.sortBy(parsed, p => {
+    let f = _.find(p.nonprofit_finances, { publishyear: publishYear - 1 });
+    return f ? f.revenue || 0 : 0;
+  }).reverse();
+  parsed = _.map(parsed, (p, pi) => {
+    let f = _.find(p.nonprofit_finances, { publishyear: publishYear - 1 });
+    p.rankPrevious = f && f.revenue ? pi + 1 : undefined;
+    return p;
+  });
+
+  // Sort and rank current year
+  parsed = _.sortBy(parsed, p => {
+    let f = _.find(p.nonprofit_finances, { publishyear: publishYear });
+    return f.revenue || 0;
+  }).reverse();
+  parsed = _.map(parsed, (p, pi) => {
+    p.rank = pi + 1;
+    return p;
+  });
+
+  // Only 100
+  parsed = _.take(parsed, 100);
+
+  return parsed;
 }
 
 // Parse contact list
@@ -53,4 +168,28 @@ function parseContactList(input) {
   }
 
   return {};
+}
+
+// Recursive clean object
+function pruneEmpty(obj, removeNotes = true) {
+  return (function prune(current) {
+    _.forOwn(current, function(value, key) {
+      if (
+        _.isUndefined(value) ||
+        _.isNull(value) ||
+        _.isNaN(value) ||
+        (!_.isDate(value) && _.isString(value) && _.isEmpty(value)) ||
+        (!_.isDate(value) && _.isObject(value) && _.isEmpty(prune(value))) ||
+        (removeNotes && key === 'notes')
+      ) {
+        delete current[key];
+      }
+    });
+    // remove any leftover undefined values from the delete
+    // operation on an array
+    if (_.isArray(current)) _.pull(current, undefined);
+
+    return current;
+  })(_.cloneDeep(obj));
+  // Do not modify the original object, create a clone instead
 }
